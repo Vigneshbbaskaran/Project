0aca8b9428858c70b7689b8d448b752887ccd999
Vigneshbbaskaran <89230718+Vigneshbbaskaran@users.noreply.github.com>
2021-11-01 01:08:32 +0530

    Update systick_km_delay.c

--- Embedded-C/i2c/i2c scan/systick_km_delay.c
+++ Embedded-C/i2c/i2c scan/systick_km_delay.c
@@ -2,7 +2,6 @@
 
 void RED_LED_INT(void)
 {
-	
 	RCC_AHB1ENR |=(0x1<<2); // enable PORT C
 	while(!(RCC_AHB1ENR & 0x4))
 	{
@@ -12,11 +11,10 @@ void RED_LED_INT(void)
 }
 
 void sys_tick_int(void)
-	{
-
-STK_VAL   = 10; // GARBAGE VALUE
-STK_CTRL |= 0X05;  // 16MHZ
-  }
+{
+	STK_VAL   = 10; // GARBAGE VALUE
+	STK_CTRL |= 0X05;  // 16MHZ
+}
 		
 /*
 void DELAY(int a)
@@ -29,19 +27,18 @@ void DELAY(int a)
 
 void delayus(int a)
 {
-	
-STK_LOAD  = 16*a; // 1microsec * a
+	STK_LOAD  = 16*a; // 1microsec * a
 	STK_VAL   = 10; // GARBAGE VALUE
 	while(!(STK_CTRL & 0X10000)) // wait untill 100msec dealy
 		{
 			;
 		}
 }
+
 void delayms(int b)
 {
-
-STK_LOAD  = 16000*b; // 1milisec * b
-		STK_VAL   = 10; // GARBAGE VALUE
+	STK_LOAD  = 16000*b; // 1milisec * b
+	STK_VAL   = 10; // GARBAGE VALUE
 	while(!(STK_CTRL & 0X10000)) // wait untill 100msec dealy
 		{
 			;
@@ -53,7 +50,6 @@ void delaysec(int a)
 	for(;a>0;a--)
 		{
 			delayms(1000);
-
 		}
 }
 	
